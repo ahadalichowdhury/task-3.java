b8b1761aa34603397c11343d408260ced788d4a1
ahadalichowdhury <smahadalichowdhury@gmail.com>
2021-10-09 20:43:06 +0600

    task-3

---
+++ src/ahadchowdhury/Cricket.java
@@ -0,0 +1,45 @@
+package ahadchowdhury;
+
+public class Cricket extends Sports{
+    String matchType;
+    int over;
+    Player player;
+
+    Cricket(String matchType, int over,Player player ){
+        this.matchType = matchType;
+        this.over = over;
+        this.player = player;
+    }
+    void display(){
+        System.out.println("MatchType :"+matchType);
+        System.out.println("Over :"+over);
+        System.out.println("Player Name :package ahadchowdhury;
+
+public class Cricket extends Sports{
+    String matchType;
+    int over;
+    Player player;
+
+    Cricket(String matchType, int over,Player player ){
+        this.matchType = matchType;
+        this.over = over;
+        this.player = player;
+    }
+    void display(){
+        System.out.println("MatchType :"+matchType);
+        System.out.println("Over :"+over);
+        System.out.println("Player Name :"+player.playerName);
+        System.out.println("jerseyNumber :"+player.jerseyNumber);
+    }
+}
+
+
+
+
+
+/*
+    Name:S.M.Ahad Ali Chowdhury
+    ID: 2012020156
+    Sec: 5D
+    email: cse_2012020156@lus.ac.bd
+     */
---
+++ src/ahadchowdhury/Main.java
@@ -0,0 +1,28 @@
+package ahadchowdhury;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Player player  = new Player("Shakib",75);
+            Cricket cricket = new Cricket("International Match",20,player);
+            cricket.display();
+            Football football = new Fpackage ahadchowdhury;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Player player  = new Player("Shakib",75);
+            Cricket cricket = new Cricket("International Match",20,player);
+            cricket.display();
+            Football football = new Football();
+        }
+    }
+
+
+
+    /*
+    Name:S.M.Ahad Ali Chowdhury
+    ID: 2012020156
+    Sec: 5D
+    email: cse_2012020156@lus.ac.bd
+     */
